8513a796fa35cd0c80122903dd7ee25d7cca085a
Lokesh Bansal <lokesh@75way.com>
2024-02-24 14:43:08 +0530

    first commit

---
+++ src/components/ImageGeneration.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import axios from "axios";
+import styles from "./ImageGeneration.module.css";
+// https://n3967h.csb.app/
+const URL = "https://api.openai.com/v1/images/generations";
+const API_KEY = "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";
+const ImageGeneration = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [prompt, setPrompt] = useState("");
+  const getImage = async () => {
+    setLoading(true);
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+        "User-Agent": "Chrome",
+      },
+    };
+    try {
+      const res = await axios.post(
+        URL,
+        {
+          prompt,
+          num_images: 1,
+          size: "1024x1024",
+        },
+        config
+      );
+      if (res.status === 200) {
+        setData(res.data.data[0].url);
+        setPrompt("");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1>ImagiGen.ai</h1>
+        <p>Unleashing Visual Imagination</p>
+        <div className={styles.imageContainer}>
+          <img src={data} className={styles.img} />
+          {!data && <p className={styles.noImage}>Generate an image</p>}
+          {loading && <div className={styles.loading}>Loading...</div>}
+        </div>
+        <div className={styles.inputContainer}>
+          <input
+            type="text"
+            placeholder="What's in your mind!"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+          />
+          <button className={styles.btn} onClick={getImage} disabled={loading}>
+            {loading ? "Generating..." : "Generate"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ImageGeneration;
